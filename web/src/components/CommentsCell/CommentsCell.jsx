a6246477bfac2cea50c263791a7a7226f51c269b
Naim-Ely21 <ne4@njit.edu>
2024-02-05 00:40:04 -0500

    First commit

---
+++ web/src/components/CommentsCell/CommentsCell.jsx
@@ -0,0 +1,31 @@
+import Comment from 'src/components/Comment'
+
+export const QUERY = gql`
+  query CommentsQuery($postId: Int!) {
+    comments(postId: $postId) {
+      id
+      name
+      body
+      postId
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => {}
+
+export const Failure = ({ error }) => (
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
+)
+
+export const Success = ({ comments }) => {
+  return (
+    <div className="space-y-8">
+      {comments.map((comment) => (
+        <Comment comment={comment} key={comment.id} />
+      ))}
+    </div>
+  )
+}
